865f627659f05dd69114d829e852d8f10887d2c9
Lallivero <larsson.9238@gmail.com>
2022-04-04 15:07:37 +0200

    Second activity created, working button implemented (though it is ugly)

---
+++ app/src/main/java/com/example/candyfisher/FishingActivity.java
@@ -0,0 +1,14 @@
+package com.example.candyfisher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FishingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fishing);
+    }
+}
